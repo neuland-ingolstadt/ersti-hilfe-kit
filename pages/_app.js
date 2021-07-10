895f3409e511f5085722b4c95c2903486e375098
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-10 20:13:07 +0200

    Add eslint-config-next and fix build

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'leaflet/dist/leaflet.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.any
 }
 
 export default MyApp
