ea89e3f6309b48c17c7b5b6bbb9a32fe651989d0
Alexander Horn <alexhorn@users.noreply.github.com>
2021-10-04 22:30:53 +0200

    Add iOS 14.6 IndexedDB workaround

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,11 @@ import 'leaflet/dist/leaflet.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 
+// workaround for this iOS 14.6 bug
+// https://bugs.webkit.org/show_bug.cgi?id=226547
+// eslint-disable-next-line no-unused-expressions
+typeof window !== 'undefined' && window.indexedDB
+
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
